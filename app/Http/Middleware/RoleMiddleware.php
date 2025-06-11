fa2fff77d468761e88823ae9790b8df11241fe07
Behnam Karimi <behnamkarimigalleh1382@gmail.com>
2025-06-11 22:33:21 +0430

    add handle function for check controller

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -5,16 +5,23 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+    /**
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  mixed  ...$roles
+     */
+
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
+    public function handle($request, Closure $next, ...$roles)
     {
+        if (!Auth::check() || !in_array(Auth::user()->role, $roles)) {
+            abort(403);
+        }
+
         return $next($request);
     }
 }
