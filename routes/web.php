38ce7e3bd1c0eed35007d671dfd9718f1d127d69
Behnam Karimi <behnamkarimigalleh1382@gmail.com>
2025-06-12 13:04:40 +0430

    add route for admin for courses

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
+use App\Http\Controllers\User\EnrollmentController;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
@@ -25,5 +29,18 @@ Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
 })->name('admin.dashboard')->middleware(['auth']);
 
+
+Route::middleware(['auth'])->group(function () {
+
+    // مسیرهای فقط ادمین
+    Route::middleware(['admin'])->prefix('admin')->group(function () {
+        Route::resource('courses', CourseController::class); // CRUD دوره‌ها
+    });
+
+    // مسیرهای کاربران عادی (user)
+    Route::get('/courses', [EnrollmentController::class, 'index'])->name('courses.index');
+    Route::post('/courses/{course}/enroll', [EnrollmentController::class, 'enroll'])->name('courses.enroll');
+    Route::get('/my-courses', [EnrollmentController::class, 'myCourses'])->name('courses.my');
+});
 
 require __DIR__.'/auth.php';
